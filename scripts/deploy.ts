381bbdc31e08c2f442b3628ad837e46ca31daeb6
Marcelo Kopmann <marcelokopmann@gmail.com>
2022-03-31 17:51:31 -0300

    feat: character creation

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -2,7 +2,16 @@ import hre from "hardhat";
 
 const main = async () => {
   const gameContractFactory = await hre.ethers.getContractFactory("MyEpicGame");
-  const gameContract = await gameContractFactory.deploy();
+  const gameContract = await gameContractFactory.deploy(
+    ["Mage", "Warrior", "Hunter"],
+    [
+      "https://i.imgur.com/5NN9BoZ.png",
+      "https://i.imgur.com/SvxBOn8.png",
+      "https://i.imgur.com/yoWSYY3.png",
+    ],
+    [100, 300, 200],
+    [100, 25, 50]
+  );
   await gameContract.deployed();
   console.log("Contract deployed to:", gameContract.address);
 };
