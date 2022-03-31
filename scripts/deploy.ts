8cd068f002d481d9e8237bcca599a7fd7837b52a
Marcelo Kopmann <marcelokopmann@gmail.com>
2022-03-31 18:11:44 -0300

    feat: character NFT

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -14,6 +14,15 @@ const main = async () => {
   );
   await gameContract.deployed();
   console.log("Contract deployed to:", gameContract.address);
+
+  // We only have three characters.
+  // an NFT w/ the character at index 2 of our array.
+  const txn = await gameContract.mintCharacterNFT(2);
+  await txn.wait();
+
+  // Get the value of the NFT's URI.
+  const returnedTokenUri = await gameContract.tokenURI(1);
+  console.log("Token URI:", returnedTokenUri);
 };
 
 const runMain = async () => {
