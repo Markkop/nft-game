8dbc1dd47fa9deac8c2ebf3a3e9c647cca29869a
Marcelo Kopmann <marcelokopmann@gmail.com>
2022-04-05 15:46:00 -0300

    feat: deploy to rinkeby

---
+++ scripts/constructorArgs.ts
@@ -0,0 +1,10 @@
+module.exports = [
+  ["Mage", "Warrior", "Hunter"],
+  [
+    "https://i.imgur.com/5NN9BoZ.png",
+    "https://i.imgur.com/SvxBOn8.png",
+    "https://i.imgur.com/yoWSYY3.png",
+  ],
+  [100, 300, 200],
+  [100, 25, 50],
+];
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,24 +1,33 @@
 import hre from "hardhat";
+const constructorArgs = require("./constructorArgs.ts");
 
 const main = async () => {
   const gameContractFactory = await hre.ethers.getContractFactory("MyEpicGame");
-  const gameContract = await gameContractFactory.deploy(
-    ["Mage", "Warrior", "Hunter"],
-    [
-      "https://i.imgur.com/5NN9BoZ.png",
-      "https://i.imgur.com/SvxBOn8.png",
-      "https://i.imgur.com/yoWSYY3.png",
-    ],
-    [100, 300, 200],
-    [100, 25, 50]
+  const gameContract = await gameContractFactory.deploy.apply(
+    null,
+    constructorArgs
   );
   await gameContract.deployed();
   console.log("Contract deployed to:", gameContract.address);
 
-  // We only have three characters.
-  // an NFT w/ the character at index 2 of our array.
-  const txn = await gameContract.mintCharacterNFT(2);
+  let txn;
+  txn = await gameContract.mintCharacterNFT(0);
   await txn.wait();
+  console.log("Minted NFT #1");
+
+  txn = await gameContract.mintCharacterNFT(1);
+  await txn.wait();
+  console.log("Minted NFT #2");
+
+  txn = await gameContract.mintCharacterNFT(2);
+  await txn.wait();
+  console.log("Minted NFT #3");
+
+  txn = await gameContract.mintCharacterNFT(1);
+  await txn.wait();
+  console.log("Minted NFT #4");
+
+  console.log("Done deploying and minting!");
 
   // Get the value of the NFT's URI.
   const returnedTokenUri = await gameContract.tokenURI(1);
